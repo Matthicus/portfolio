2c7ab9452ea7575667e899cca2a4f1d854e32f02
Matthice Storms <matthice.storms@hotmail.com>
2026-05-21 19:02:40 +0200

    tab text changing

--- src/App.jsx
+++ src/App.jsx
@@ -6,8 +6,21 @@ import TextFlip from "./components/TextFlip";
 import Footer from "./components/Footer";
 import About from "./components/About";
 import Work from "./components/Work";
+import { useEffect } from "react";
 
 function App() {
+  useEffect(() => {
+    const titles = ["Storms Matthice", "Webdesign", "Graphic design"];
+
+    const index = { current: 0 };
+
+    const interval = setInterval(() => {
+      index.current = (index.current + 1) % titles.length;
+      document.title = titles[index.current];
+    }, 2000);
+
+    return () => clearInterval(interval);
+  }, []);
   return (
     <BrowserRouter>
       <div className="App w-full overflow-x-hidden">
